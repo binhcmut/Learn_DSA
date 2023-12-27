174e957bae9e1256dfb239463f774cdf636712ac
bin <120003822+binhcmut@users.noreply.github.com>
2023-12-27 10:09:29 +0700

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,18 @@
+def print_str(s):
+    print(s)
+
+# bai 1
+print("hello world")
+
+# bai 2
+str_value = "hello world"
+print(str_value)
+
+# bai 3
+s1 = "hello"
+s2 = "world"
+s3 = s1 + " " + s2
+print(s3)
+
+# bai 4
+print_str(s3)
